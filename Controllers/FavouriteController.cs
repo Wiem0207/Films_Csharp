fa688b2248317c20711b6559edf06565f1656f07
Wiem0207 <wiembembli72@gmail.com>
2024-11-29 13:14:03 +0100

    FavouritesControllers terminé

--- Controllers/FavouriteController.cs
+++ Controllers/FavouriteController.cs
@@ -8,55 +8,86 @@ using System.Threading.Tasks;
 
 namespace WebApi.Controllers
 {
-        [ApiController]
-        [Route("api/Favourite")]
-        public class FavouriteController : ControllerBase
-        {
-            private readonly BddContext _context;
+    [ApiController]
+    [Route("api/Favourite")]
+    public class FavouriteController : ControllerBase
+    {
+        private readonly BddContext _context;
 
-            public FavouriteController(BddContext context)
+        public FavouriteController(BddContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/Favourite/add
+        [HttpPost("add")]
+        public async Task<ActionResult> AddFavourite(int filmId)
+        {
+            int userId = 1;
+
+            // Vérifier si le film existe
+            var film = await _context.Films.FindAsync(filmId);
+            if (film == null)
             {
-                _context = context;
+                return NotFound("Film non trouvé.");
             }
 
-            // POST api/Favourite/add
-            [HttpPost("add")]
-            public async Task<ActionResult> AddFavourite([FromBody] int filmId)
+            // Vérifier si le film est déjà dans les favoris de l'utilisateur
+            var existingFavourite = await _context.Favourites
+                .FirstOrDefaultAsync(f => f.UserId == userId && f.FilmId == filmId);
+            if (existingFavourite != null)
             {
-            int userId = 1;
-             
-                // Vérifier si le film existe
-                var film = await _context.Films.FindAsync(filmId);
-                if (film == null)
-                {
-                    return NotFound("Film non trouvé.");
-                }
-
-                // Vérifier si le film est déjà dans les favoris de l'utilisateur
-                var existingFavourite = await _context.Favourites
-                    .FirstOrDefaultAsync(f => f.UserId == userId && f.FilmId == filmId);
-                if (existingFavourite != null)
-                {
-                    return Conflict("Ce film est déjà dans les favoris.");
-                }
+                return Conflict("Ce film est déjà dans les favoris.");
+            }
             int nextId = (_context.Favourites.Any())
              ? _context.Favourites.Max(f => f.Id) + 1
              : 1;
 
             // Ajouter aux favoris
             var favourite = new Favourite
-                {
-                    UserId = userId,
-                    FilmId = filmId,
-                    Id= nextId
-                };
+            {
+                UserId = userId,
+                FilmId = filmId,
+                Id = nextId
+            };
 
-                _context.Favourites.Add(favourite);
-                await _context.SaveChangesAsync();
+            _context.Favourites.Add(favourite);
+            await _context.SaveChangesAsync();
 
-                return Ok("Film ajouté aux favoris avec succès.");
-            }
+            return Ok("Film ajouté aux favoris avec succès.");
         }
+        [HttpDelete("{filmId}")]
+        public async Task<IActionResult> Deletefavourite(int filmId)
+        {
+            int userId = 1;
+            // on récupère la confiture que l'on souhaite supprimer
+            Favourite favourite = await _context.Favourites.FirstOrDefaultAsync(f => f.UserId == userId && f.FilmId == filmId);
+            if (favourite == null)
+            {
+                return NotFound($"Le film avec l'ID {filmId} n'est pas dans les favoris de l'utilisateur {userId}.");
+            }
+            // on indique a notre contexte que l'objet a été supprimé
+            _context.Favourites.Remove(favourite);
+            // on enregistre les modifications
+            await _context.SaveChangesAsync();
+            // on retourne un code 204 pour indiquer que la suppression a bien eu lieu
+            return Ok($"Le film avec l'ID {filmId} a été supprimé des favoris de l'utilisateur {userId}.");
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Favourite>>> GetFavourites()
+        {
+            int userID = 1;
+            var favouriteFilmIds = await _context.Favourites
+            .Where(f => f.UserId == userID)  // Filtrer par l'utilisateur
+            .Select(f => f.FilmId)  // Sélectionner uniquement les FilmId
+            .ToListAsync();
 
+            if (favouriteFilmIds == null || favouriteFilmIds.Count == 0)
+            {
+                return NotFound("Aucun film trouvé dans les favoris.");
+            }
+
+            return Ok(favouriteFilmIds);
+        }
+    }
 }
-
--- Controllers/FilmController.cs
+++ Controllers/FilmController.cs
@@ -17,6 +17,7 @@ namespace WebApi.Controllers
         {
             _context = context;
         }
+        //pour recuperer une liste de tt les films
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Film>>> GetFilms()
         {
@@ -114,100 +115,5 @@ namespace WebApi.Controllers
         }
     }
 }
-//public class UserCreation
-//{
-//    public string Pseudo { get; set; }
-//    public string Password { get; set; }
-//    public Role Role { get; set; }
-//}
-//[HttpPost]
-//public async Task<ActionResult<User>> PostUser(UserCreation userCreation)
-//{
-//    // on créer une nouvelle confiture avec les informations reçu
-//    User user = new User
-//    {
-//        Pseudo = userCreation.Pseudo,
-//        Role = userCreation.Role
-//    };
-//    // Hachage du mot de passe avant de le stocker
-//    user.Password = _passwordHasher.HashPassword(user, userCreation.Password);
-//    // on l'ajoute a notre contexte (BDD)
-//    _context.Users.Add(user);
-//    // on enregistre les modifications dans la BDD ce qui remplira le champ Id de notre objet
-//    await _context.SaveChangesAsync();
-//    // on retourne un code 201 pour indiquer que la création a bien eu lieu
-//    return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
-//}
-//[HttpPut("{id}")]
-//public async Task<IActionResult> PutUser(int id, User userUpdate)
-//{
-//    // on récupère la confiture que l'on souhaite modifier
-//    User user = await _context.Users.FindAsync(userUpdate.Id);
-//    if (user == null)
-//    {
-//        return NotFound();
-//    }
-
-//    // on met a jour les informations de la confiture
-//    user.Pseudo = userUpdate.Pseudo;
-//    user.Password = userUpdate.Password;
-//    user.Role = userUpdate.Role;
-//    // on indique a notre contexte que l'objet a été modifié
-//    _context.Entry(user).State = EntityState.Modified;
-
-//    try
-//    {
-//        // on enregistre les modifications
-//        await _context.SaveChangesAsync();
-//    }
-//    catch (DbUpdateConcurrencyException)
-//    {
-//        // si une erreur de concurrence survient on retourne un code 500
-//        return StatusCode(500, "Erreur de concurrence");
-//    }
-//    // on retourne un code 200 pour indiquer que la modification a bien eu lieu
-//    return Ok(user);
-//}
-//[HttpDelete("{id}")]
-//public async Task<IActionResult> DeleteUser(int id)
-//{
-//    // on récupère la confiture que l'on souhaite supprimer
-//    User user = await _context.Users.FindAsync(id);
-//    if (user == null)
-//    {
-//        return NotFound();
-//    }
-//    // on indique a notre contexte que l'objet a été supprimé
-//    _context.Users.Remove(user);
-//    // on enregistre les modifications
-//    await _context.SaveChangesAsync();
-//    // on retourne un code 204 pour indiquer que la suppression a bien eu lieu
-//    return NoContent();
-//}
-//[HttpPost("login")]
-//public async Task<ActionResult<User>> Login([FromBody] UserInfo userInfo)
-//{
-//    // Recherche de l'utilisateur avec le pseudo
-//    var user = await _context.Users.SingleOrDefaultAsync(u => u.Pseudo == userInfo.Pseudo);
-//    if (user == null)
-//    {
-//        return Unauthorized("Utilisateur non trouvé");
-//    }
-
-//    // Vérification du mot de passe en le comparant avec le mot de passe haché stocké en base
-//    var result = _passwordHasher.VerifyHashedPassword(user, user.Password, userInfo.Password);
-//    if (result == PasswordVerificationResult.Failed)
-//    {
-//        return Unauthorized("Mot de passe incorrect");
-//    }
-
-//    // Si l'utilisateur est authentifié, on retourne l'utilisateur
-//    return Ok(user);
-//}
-
-//    }
-//    ///////////////
-
-//}
 
 
